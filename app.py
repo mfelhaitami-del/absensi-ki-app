d2000908aea060d165b087c656755e1df1bbd345
mfelhaitami-del <mfelhaitami@gmail.com>
2026-02-09 17:22:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from io import BytesIO
 
 # --- 1. KONFIGURASI ---
 API_IMGBB = "4c3fb57e24494624fd12e23156c0c6b0"
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwKEM1F-kthxUFjIt_qc11U-98NEPLqD4g7nyl7TbHUA7H3QLSjchYC8U8bxSOxtuxM/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycby-q43OJig9ToecMqU3h7UOHygWNlyqNdYLFrhdXs5e6GS5IFkcKL4FiPSk97cznssn/exec"
 
 st.set_page_config(page_title="Absensi Tim KI", layout="wide")
 
